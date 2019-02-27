e71b7613b9caf9504acf6f921e802a701d0c5385
nirberko <nirberko@gmail.com>
2019-02-27 21:52:08 +0200

    improve css and structure

--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -29,79 +29,96 @@ const fetchThumbnails = (user_id) => {
 const urlParams = new URLSearchParams(window.location.search);
 fetchThumbnails(urlParams.get('user_id'));
 
+const Thumbnail = item => {
+    const ThumbnailCard = new Component();
+
+    const matchAuthor = item.author.match(/\("(.+)"\)/);
+
+    const header = (
+        createElement('header', {
+            attrs: {
+                class: 'Thumbnails__list__card__details__header'
+            },
+            children: [
+                createElement('h3', {
+                    events: {
+                        onclick: () => window.open(item.link, '_blank')
+                    },
+                    children: [item.title]
+                }),
+                createElement('button', {
+                    events: {
+                        onclick: (e) => {
+                            e.stopPropagation();
+                            history.pushState({}, null, `?user_id=${item.author_id}`);
+                            fetchThumbnails(item.author_id)
+                        }
+                    },
+                    children: [matchAuthor[1]]
+                })
+            ]
+        })
+    );
+
+    const footer = (
+        createElement('footer', {
+            attrs: {
+                class: 'Thumbnails__list__card__details__dateTaken'
+            },
+            children: [
+                dayjs(item.date_taken).format('DD MMMM YYYY')
+            ]
+        })
+    );
+
+    ThumbnailCard.render(() => (
+        createElement('div', {
+            attrs: {
+                class: 'Thumbnails__list__card'
+            },
+            children: [
+                createElement('img', {
+                    attrs: {
+                        src: item.media.m
+                    }
+                }),
+                createElement('article', {
+                    attrs: {
+                        class: 'Thumbnails__list__card__details'
+                    },
+                    children: [
+                        header,
+                        footer
+                    ]
+                })
+            ]
+        })
+    ));
+
+    return ThumbnailCard;
+};
+
 Thumbnails.render(({thumbnails}) => {
-    let children = thumbnails.length ? [] : [
+    let children =  [
         Loader
     ];
 
+    let columns = [[],[],[],[]];
+
+    let i = 0;
     thumbnails.forEach(item => {
-        const ThumbnailCard = new Component();
+        columns[i].push(Thumbnail(item).render());
 
-        const matchAuthor = item.author.match(/\("(.+)"\)/);
-
-        const header = (
-            createElement('header', {
-                attrs: {
-                    class: 'Thumbnails__list__card__details__header'
-                },
-                children: [
-                    createElement('h3', {
-                        events: {
-                            onclick: () => window.open(item.link, '_blank')
-                        },
-                        children: [item.title]
-                    }),
-                    createElement('button', {
-                        events: {
-                            onclick: (e) => {
-                                e.stopPropagation();
-                                history.pushState({}, null, `?user_id=${item.author_id}`);
-                                fetchThumbnails(item.author_id)
-                            }
-                        },
-                        children: [matchAuthor[1]]
-                    })
-                ]
-            })
-        );
-
-        const footer = (
-            createElement('footer', {
-                attrs: {
-                    class: 'Thumbnails__list__card__details__dateTaken'
-                },
-                children: [
-                    dayjs(item.date_taken).format('DD MMMM YYYY')
-                ]
-            })
-        );
-
-        ThumbnailCard.render(() => (
-            createElement('li', {
-                attrs: {
-                    class: 'Thumbnails__list__card'
-                },
-                children: [
-                    createElement('img', {
-                        attrs: {
-                            src: item.media.m
-                        }
-                    }),
-                    createElement('article', {
-                        attrs: {
-                            class: 'Thumbnails__list__card__details'
-                        },
-                        children: [
-                            header,
-                            footer
-                        ]
-                    })
-                ]
-            })
-        ));
-
-        children.push(ThumbnailCard.render())
+        i === 3 ? i = 0 : i++
     });
+
+    if (thumbnails.length) {
+        children = columns.map(column => (
+            createElement('li', {
+                children: column
+            })
+        ))
+    }
 
     return (
         createElement('div', {
